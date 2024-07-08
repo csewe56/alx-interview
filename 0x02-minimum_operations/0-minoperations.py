5d5d17e93f38864985cfdf89f190b7569563a3a0
csewe56 <csewe56@gmail.com>
2024-07-08 05:14:00 -0700

    0x02. Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Minimum Operations
+"""
+
+
+def minOperations(n):
+    """
+    Calculates the fewest number of operations needed to result in exactly n H characters in the file.
+
+    Parameters:
+    n (int): The target number of H characters.
+
+    Returns:
+    int: The minimum number of operations needed, or 0 if n is impossible to achieve.
+    """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
